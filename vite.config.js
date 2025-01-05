bd0df71d89934f05c8c81e02b453dc7957e8e013
amey146 <108222636+amey146@users.noreply.github.com>
2025-01-05 14:18:25 +0530

    update3

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@fortawesome/free-solid-svg-icons'],
+      external: ['@fortawesome/free-solid-svg-icons','@fortawesome/react-fontawesome','date-fns'],
     },
   },
 })
